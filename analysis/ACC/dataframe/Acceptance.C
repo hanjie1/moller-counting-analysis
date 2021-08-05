f0e39dc9cfe4a1706b628673e630a52051a9dc34
hanjie1 <hanjie@jlab.org>
2021-08-05 17:41:37 -0400

    plots

--- analysis/ACC/dataframe/Acceptance.C
+++ analysis/ACC/dataframe/Acceptance.C
@@ -14,8 +14,8 @@ void Acceptance(){
    ROOT::RDataFrame d(treeName, fileName);
 
    // cuts
-   auto main_el="hit.det==28 && hit.Pid==11 && hit.mtrid==0 && hit.trid==1";   // elastic primary events at main detector
-   auto main_moller="hit.det==28 && hit.Pid==11 && hit.mtrid==0 && (hit.trid==1 || hit.trid==2)";   // moller primary events at maine detector
+   auto main_el="hit.det==28 && hit.pid==11 && hit.mtrid==0 && hit.trid==1";   // elastic primary events at main detector
+   auto main_moller="hit.det==28 && hit.pid==11 && hit.mtrid==0 && (hit.trid==1 || hit.trid==2)";   // moller primary events at maine detector
 
    // apply cuts and generate new columns
    auto selected_d = d.Define("maindet_hit",main_moller).Define("good_hit","Sum(maindet_hit)").Define("trid","hit.trid[maindet_hit]").Filter("good_hit>0");
@@ -35,25 +35,40 @@ void Acceptance(){
    // make plots
    SetXYPhi();
 
-   ::Info("", "Elapsed time 1:  %f, sec", stpw->RealTime());
-   std::cout << stpw->RealTime() << std::endl;
+   std::cout <<"1:  "<< stpw->RealTime() << std::endl;
+   stpw->Start();
 
-   ROOT::RDF::RResultPtr<TH1D> tgth_in[6][7][4];
+   ROOT::RDF::RResultPtr<TH1D> tgth_in;
    ROOT::RDF::RResultPtr<TH1D> tgth_main[6][7][4];
    TH1D *tgth_acc[6][7][4];
 
-   ROOT::RDF::RResultPtr<TH1D> tgph_in[6][7][4];
+   ROOT::RDF::RResultPtr<TH1D> tgph_in;
    ROOT::RDF::RResultPtr<TH1D> tgph_main[6][7][4];
    TH1D *tgph_acc[6][7][4];
 
-   ROOT::RDF::RResultPtr<TH1D> tgp_in[6][7][4];
+   ROOT::RDF::RResultPtr<TH1D> tgp_in;
    ROOT::RDF::RResultPtr<TH1D> tgp_main[6][7][4];
    TH1D *tgp_acc[6][7][4];
+
+   ROOT::RDF::RResultPtr<TH2D> tg_thph_in;
+   ROOT::RDF::RResultPtr<TH2D> tg_thph_main[6][7][4];
+   TH2D *tg_thph_acc[6][7][4];
+
+   ROOT::RDF::RResultPtr<TH2D> tg_thp_in;
+   ROOT::RDF::RResultPtr<TH2D> tg_thp_main[6][7][4];
+   TH2D *tg_thp_acc[6][7][4];
+
+
+   tgth_in = d.Histo1D<Double_t>({"tgth_in","tgth_in",50,0,0.024},"part.th","rate");
+   tgph_in = d.Histo1D<Double_t>({"tgph_in","tgph_in",50,-Pi(),Pi()},"part.ph","rate");
+   tgp_in = d.Histo1D<Double_t>({"tgp_in","tgp_in",50,0,11000},"part.p","rate");
+
+   tg_thph_in = d.Histo2D<Double_t>({"tg_thph_in","tg_thph_in",50,0,0.024,50,-Pi(),Pi()},"part.th","part.ph","rate");
+   tg_thp_in = d.Histo2D<Double_t>({"tg_thp_in","tg_thp_in",50,0,0.024,50,0,11000},"part.th","part.p","rate");
 
    for(int ii=0; ii<6; ii++){
     for(int jj=0; jj<7; jj++){
      for(int kk=0; kk<4; kk++){
-
 	TString cuts;
         if(jj==3 && kk==2) 
 	   cuts = Form("(main_r>%f && main_r<%f) && ((main_ph>%f && main_ph<%f) || (main_ph>%f && main_ph<%f))",r[ii],r[ii+1],ph[jj][kk],Pi(),-Pi(),ph[jj][kk+1]);
@@ -63,37 +78,91 @@ void Acceptance(){
 	auto sec_df = newdf.Filter(cuts.Data());
 
 	// target theta acceptance
-	tgth_in[ii][jj][kk] = d.Histo1D<Double_t>({Form("tgth_in_r%ds%dp%d",ii+1,jj+1,kk+1),Form("tgth_in_r%ds%dp%d",ii+1,jj+1,kk+1),50,0,0.024},"part.th","rate");
-	tgth_main[ii][jj][kk] = sec_df.Histo1D<Double_t>({Form("tgth_acc_r%ds%dp%d",ii+1,jj+1,kk+1),Form("tgth_acc_r%ds%dp%d",ii+1,jj+1,kk+1),50,0,0.024},"tg_th","rate");
+	tgth_main[ii][jj][kk] = sec_df.Histo1D<Double_t>({Form("tgth_main_r%ds%dp%d",ii+1,jj+1,kk+1),Form("tgth_main_r%ds%dp%d",ii+1,jj+1,kk+1),50,0,0.024},"tg_th","rate");
 	
-	tgth_acc[ii][jj][kk] = (TH1D *)tgth_main[ii][jj][kk].GetPtr()->Clone();
-	tgth_acc[ii][jj][kk]->Divide(tgth_in[ii][jj][kk].GetPtr());	
-
 	// target phi acceptance
-	tgph_in[ii][jj][kk] = d.Histo1D<Double_t>({Form("tgph_in_r%ds%dp%d",ii+1,jj+1,kk+1),Form("tgph_in_r%ds%dp%d",ii+1,jj+1,kk+1),50,-Pi(),Pi()},"part.ph","rate");
-	tgph_main[ii][jj][kk] = sec_df.Histo1D<Double_t>({Form("tgph_acc_r%ds%dp%d",ii+1,jj+1,kk+1),Form("tgph_acc_r%ds%dp%d",ii+1,jj+1,kk+1),50,-Pi(),Pi()},"tg_ph","rate");
+	tgph_main[ii][jj][kk] = sec_df.Histo1D<Double_t>({Form("tgph_main_r%ds%dp%d",ii+1,jj+1,kk+1),Form("tgph_main_r%ds%dp%d",ii+1,jj+1,kk+1),50,-Pi(),Pi()},"tg_ph","rate");
 	
-	tgph_acc[ii][jj][kk] = (TH1D *)tgph_main[ii][jj][kk].GetPtr()->Clone();
-	tgph_acc[ii][jj][kk]->Divide(tgph_in[ii][jj][kk].GetPtr());	
-
-
 	// target p acceptance
-	tgp_in[ii][jj][kk] = d.Histo1D<Double_t>({Form("tgp_in_r%ds%dp%d",ii+1,jj+1,kk+1),Form("tgp_in_r%ds%dp%d",ii+1,jj+1,kk+1),200,0,11000},"part.p","rate");
-	tgp_main[ii][jj][kk] = sec_df.Histo1D<Double_t>({Form("tgp_acc_r%ds%dp%d",ii+1,jj+1,kk+1),Form("tgp_acc_r%ds%dp%d",ii+1,jj+1,kk+1),200,0,11000},"tg_p","rate");
-	
-	tgp_acc[ii][jj][kk] = (TH1D *)tgp_main[ii][jj][kk].GetPtr()->Clone();
-	tgp_acc[ii][jj][kk]->Divide(tgp_in[ii][jj][kk].GetPtr());	
+	tgp_main[ii][jj][kk] = sec_df.Histo1D<Double_t>({Form("tgp_main_r%ds%dp%d",ii+1,jj+1,kk+1),Form("tgp_main_r%ds%dp%d",ii+1,jj+1,kk+1),50,0,11000},"tg_p","rate");
 
-        ::Info("", "Elapsed time 11:  %f, sec", stpw->RealTime());
-        std::cout << stpw->RealTime() << std::endl;
+	// ph vs. th
+	tg_thph_main[ii][jj][kk] = sec_df.Histo2D<Double_t>({Form("tg_thph_main_r%ds%dp%d",ii+1,jj+1,kk+1),Form("tg_thph_main_r%ds%dp%d",ii+1,jj+1,kk+1),50,0,0.024,50,-Pi(),Pi()},
+								"tg_th","tg_ph","rate");
+
+	// p vs. th
+	tg_thp_main[ii][jj][kk] = sec_df.Histo2D<Double_t>({Form("tg_thp_main_r%ds%dp%d",ii+1,jj+1,kk+1),Form("tg_thp_main_r%ds%dp%d",ii+1,jj+1,kk+1),50,0,0.024,50,0,11000},
+								"tg_th","tg_p","rate");
      }
     }
    }
 
-   //::Info("", "Elapsed time 2:  %f, sec", stpw->RealTime());
-   //std::cout << stpw->RealTime() << std::endl;
 
-   gStyle->SetOptStat(11001111);
+/*
+   TH1D *test=new TH1D("test","test",50,0,0.024);
+   for(int ii=1; ii<=50; ii++){
+	Double_t n_in = tgth_in.GetPtr()->GetBinContent(ii);
+	Double_t n_main = tgth_main[5][0][0].GetPtr()->GetBinContent(ii);
+
+	Double_t ratio = 0;
+	if(n_in!=0) ratio=n_main/n_in;
+	test->SetBinContent(ii,ratio);	
+   }
+
+   test->Draw();
+*/
+
+   std::cout <<"2:  "<< stpw->RealTime() << std::endl;
+   stpw->Start();
+
+   // acceptance plots
+   
+   for(int ii=0; ii<6; ii++){
+    for(int jj=0; jj<7; jj++){
+     for(int kk=0; kk<4; kk++){
+	tgth_acc[ii][jj][kk] = (TH1D *)tgth_main[ii][jj][kk].GetPtr()->Clone();
+	tgth_acc[ii][jj][kk]->Divide(tgth_in.GetPtr());	
+
+	tgph_acc[ii][jj][kk] = (TH1D *)tgph_main[ii][jj][kk].GetPtr()->Clone();
+	tgph_acc[ii][jj][kk]->Divide(tgph_in.GetPtr());	
+
+	tgp_acc[ii][jj][kk] = (TH1D *)tgp_main[ii][jj][kk].GetPtr()->Clone();
+	tgp_acc[ii][jj][kk]->Divide(tgp_in.GetPtr());	
+
+	tg_thph_acc[ii][jj][kk] = (TH2D *)tg_thph_main[ii][jj][kk].GetPtr()->Clone();
+	tg_thph_acc[ii][jj][kk]->Divide(tg_thph_in.GetPtr());	
+
+	tg_thp_acc[ii][jj][kk] = (TH2D *)tg_thp_main[ii][jj][kk].GetPtr()->Clone();
+	tg_thp_acc[ii][jj][kk]->Divide(tg_thp_in.GetPtr());	
+     }
+    }
+   } 
+
+   // merge different sections' acceptance together for each ring
+/*
+   TH1D *tgth_acc_r[6];
+   TH1D *tgph_acc_r[6];
+   TH1D *tgp_acc_r[6];
+   
+   TH2D *tgthph_acc_r[6];
+   TH2D *tgthp_acc_r[6];
+
+   TList *list = new TList;
+
+  list->Add(h1);
+  list->Add(h2);
+  list->Add(h3);
+  TH1F *h = (TH1F*)h1->Clone("h");
+  h->Reset();
+  h->Merge(list);
+  h->Draw();
+   
+   for
+*/
+   std::cout <<"3:  "<< stpw->RealTime() << std::endl;
+   stpw->Start();
+
+   gStyle->SetOptStat(111101);
 
    TCanvas *c1 = new TCanvas();
    c1->Divide(4,3);
@@ -101,6 +170,7 @@ void Acceptance(){
 	c1->cd(ii+1);
         tgth_acc[5][0][ii]->Draw("hist");
    }
+
    for(int ii=0; ii<4; ii++){
 	c1->cd(ii+5);
         tgph_acc[5][0][ii]->Draw("hist");
@@ -110,45 +180,9 @@ void Acceptance(){
         tgp_acc[5][0][ii]->Draw("hist");
    }
 
-   c1->Print("plots/accep.pdf");
+   c1->Print("plots/accep.pdf[");
 
-  ::Info("", "Elapsed time 3:  %f, sec", stpw->RealTime());
-  std::cout << stpw->RealTime() << std::endl;
-
-// = d.Histo1D();
-
- 
-/*   double r[7]={650,690,735,790,900,1060,1160};
-
-   auto ring_th = new THIF[6];
-   auto ring_ph = new THIF[6];
-   auto ring_p = new THIF[6];
-
-   for(int ii=0; ii<7; ii++){
-      auto c1=new TCanvas();
-      c1->Divide(4,1);
-      
-   }
-*/
- //  auto hitdet = selected_d.Histo1D("tgth");
-
-   //auto c1 = new TCanvas();
-   //hitdet->DrawCopy();
-   //auto d1=selected_d.Display({"part.th","hit.r","hit.det","maindet_hit","good_hit","trid","tgth"},7);
-   //auto d1=d.Display({"part.th","hit.r","hit.det"},7);
-   //d1->Print();
-   
-   //auto d1=check_d.Display({"part.th","hit.r","hit.det","maindet_hit","good_hit","trid"},7);
-   //auto d1=d.Display({"part.th","hit.r","hit.det"},7);
-   //d1->Print();
-
-   //auto colType = selected_d.GetColumnType("trid");
-// Print column type
-   //std::cout << "Column " << colType << " has type " << colType << std::endl;
-
-//auto colNames = d.GetColumnNames();
-// Print columns' names
-//for (auto &&colName : colNames) std::cout << colName << std::endl;
+  std::cout <<"4:  "<< stpw->RealTime() << std::endl;
 
 
 }
